9abde4f32e88387531cf99905b46e8808a3987d3
andreuchiha <andrebarn04@gmail.com>
2026-08-24 13:31:13 +0100

    React Router + Protected Routes
    - Created Example Pages
    - Set up React Router to be able to access the pages
    - Set up Protected Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
-import Exercises from "./pages/Exercises";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Exercises from "./pages/Exercises";
+import Workouts from "./pages/Workouts";
+import Nutrition from "./pages/Nutrition";
+import Progress from "./pages/Progress";
+
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
     return (
-        <Login />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+
+                <Route path="/dashboard" element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>} />
+                <Route path="/exercises" element={<ProtectedRoute> <Exercises /> </ProtectedRoute>} />
+                <Route path="/workouts" element={<ProtectedRoute> <Workouts /> </ProtectedRoute>} />
+                <Route path="/nutrition" element={<ProtectedRoute> <Nutrition /> </ProtectedRoute>} />
+                <Route path="/progress" element={<ProtectedRoute> <Progress /> </ProtectedRoute>} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ frontend/src/pages/Nutrition.jsx
@@ -0,0 +1,5 @@
+function Nutrition() {
+    return <h1>Nutrition</h1>;
+}
+
+export default Nutrition;
