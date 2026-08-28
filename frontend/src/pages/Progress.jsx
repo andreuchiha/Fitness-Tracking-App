06ad5a1af69d562e07313e7d96cfe49eb88d7536
andreuchiha <andrebarn04@gmail.com>
2026-08-28 19:15:13 +0100

    Progress Page Update 1
    Added a working weight logger to the progress page.

--- frontend/src/pages/Progress.jsx
+++ frontend/src/pages/Progress.jsx
@@ -1,5 +1,72 @@
+import styles from "../static/style.module.css";
+import { useState } from "react";
+import { createWeightLog } from "../services/progressService";
+
+
 function Progress() {
-    return <h1>Progress</h1>;
+
+
+    const [date, setDate] = useState("");
+    const [weight, setWeight] = useState("");
+    const [notes, setNotes] = useState("");
+
+    const handleSubmit = async () => {
+
+    const weightLog = {
+        date: date,
+        weight: weight,
+        notes: notes
+    };
+
+    try {
+        const response = await createWeightLog(weightLog);
+
+        console.log("Weight log created:", response);
+
+    } catch (error) {
+        console.error("Failed to create weight log:", error);
+    }
+};
+
+    return (
+
+        <div className = {styles.progressPage}>
+            
+        <h1>Progress</h1>
+
+
+            <div className = {styles.WeightLoggerContainer}>
+            
+            <span className = {styles.WeightLoggerText}> Weight Logger </span>
+
+                <div>
+                <label className = {styles.WeightLoggerText} >Date </label>
+                <input  className = {styles.WeightLoggerInput} type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+                </div>
+
+                <div>
+                <label className = {styles.WeightLoggerText} >Weight (kg)</label>
+                <input  className = {styles.WeightLoggerInput} type="number" step="0.1" value = {weight} onChange = {(e) => setWeight(e.target.value)}/>
+                </div>
+
+
+                <div>
+                <label className = {styles.WeightLoggerText} >Notes</label>
+                <input  className = {styles.WeightLoggerInput} type="text" value = {notes} onChange = {(e) => setNotes(e.target.value)}/>
+                </div>
+
+
+                <button className = {styles.WeightLoggerButton} onClick = {handleSubmit}>Submit</button>
+        
+            </div>
+
+
+            
+        </div>
+
+
+    );
+
 }
 
 export default Progress;
---
+++ frontend/src/services/progressService.js
@@ -0,0 +1,15 @@
+import api from "./api";
+
+export const getWeightLogs = async () => {
+    const response = await api.get("/api/progress/weightlogs/");
+    return response.data;
+};
+
+export const createWeightLog = async (weightLog) => {
+    const response = await api.post(
+        "progress/weightlogs/",
+        weightLog
+    );
+
+    return response.data;
+};
